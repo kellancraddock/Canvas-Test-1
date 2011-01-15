f3a630da38766dc55965291d8997d955a9d91bc5
Kellan <kellancraddock@gmail.com>
2011-01-14 18:01:56 -0800

    added processing.js and index file. implimented clock example and drag and drop

---
+++ js/helpers/canvas.js
@@ -0,0 +1,62 @@
+$(function() {
+	function sketchProc(processing) {
+		//DEFAULT SETUP FUNCTION
+		processing.setup = function() {
+			processing.size(640, 360);
+			mX = processing.width / 2;  
+  			mY = processing.height / 2;
+		}
+		//DEFAULT DRAW FUNCTION (loops every 60sec by default)	 
+		processing.draw = function() {	
+			// determine center and max clock arm length	
+			var centerX = processing.width / 2, centerY = processing.height / 2;	
+			var maxArmLength = Math.min(centerX, centerY);
+			
+				
+		
+			function drawArm(position, lengthScale, weight) {	 
+				processing.strokeWeight(weight);	 
+				processing.line(centerX, centerY,  
+					 centerX + Math.sin(position * 2 * Math.PI) * lengthScale * maxArmLength,	
+					 centerY - Math.cos(position * 2 * Math.PI) * lengthScale * maxArmLength);	 
+					 
+				/*
+processing.strokeWeight(10);
+				processing.ellipse(55, 55, 55, 55);
+*/
+			}
+			
+			processing.mouseDragged = function() {
+				mX = processing.mouseX;
+				mY = processing.mouseY;
+			}
+			
+		
+			// RESETS THE BACKGROUND
+			processing.background(224);	 
+			
+			//Draw a circle
+			processing.strokeWeight(10);
+			processing.ellipse(mX, mY, 55, 55);
+			
+			var now = new Date();	 
+		
+			// Moving hours arm by small increments	 
+			var hoursPosition = (now.getHours() % 12 + now.getMinutes() / 60) / 12;	 
+			drawArm(hoursPosition, 0.5, 5);	 
+		
+			// Moving minutes arm by small increments	 
+			var minutesPosition = (now.getMinutes() + now.getSeconds() / 60) / 60;	
+			drawArm(minutesPosition, 0.80, 3);	
+		
+			// Moving hour arm by second increments	 
+			var secondsPosition = now.getSeconds() / 60;	
+			drawArm(secondsPosition, 0.90, 1);
+		
+		};	
+	}	 
+		
+	var canvas = document.getElementById("canvas1"); 
+	// attaching the sketchProc function to the canvas	 
+	var processingInstance = new Processing(canvas, sketchProc);
+});
