47803392f2762e6b2ffcc553dec20f10eaa9a6fa
Kellan <kellancraddock@gmail.com>
2011-01-17 11:41:11 -0800

    practice changing shape radius and stroke. loaded image

--- js/helpers/canvas.js
+++ js/helpers/canvas.js
@@ -1,43 +1,55 @@
 $(function() {
 	function sketchProc(processing) {
+		var P = processing;
+		var b = P.PImage;
 		//DEFAULT SETUP FUNCTION
-		processing.setup = function() {
-			processing.size(640, 360);
-			mX = processing.width / 2;  
-  			mY = processing.height / 2;
+		P.setup = function() {
+			P.size(1024, 768);
+			mX = P.width / 2;  
+  			mY = P.height / 2;
+  			// @pjs preload must be used to preload the image
+   			/* @pjs preload="/images/CN70_360Tour_025T.jpg"; */
+   			/* var b = P.PImage; */
+    		b = P.loadImage("/images/CN70_360Tour_025T.jpg");
 		}
 		//DEFAULT DRAW FUNCTION (loops every 60sec by default)	 
-		processing.draw = function() {	
+		P.draw = function() {	
 			// determine center and max clock arm length	
-			var centerX = processing.width / 2, centerY = processing.height / 2;	
+			var centerX = P.width / 2, centerY = P.height / 2;	
 			var maxArmLength = Math.min(centerX, centerY);
 			
 				
 		
 			function drawArm(position, lengthScale, weight) {	 
-				processing.strokeWeight(weight);	 
-				processing.line(centerX, centerY,  
+				P.strokeWeight(weight);	 
+				P.line(centerX, centerY,  
 					 centerX + Math.sin(position * 2 * Math.PI) * lengthScale * maxArmLength,	
 					 centerY - Math.cos(position * 2 * Math.PI) * lengthScale * maxArmLength);	 
 					 
 				/*
-processing.strokeWeight(10);
-				processing.ellipse(55, 55, 55, 55);
+P.strokeWeight(10);
+				P.ellipse(55, 55, 55, 55);
 */
 			}
 			
-			processing.mouseDragged = function() {
-				mX = processing.mouseX;
-				mY = processing.mouseY;
+			P.mouseDragged = function() {
+				mX = P.mouseX;
+				mY = P.mouseY;
 			}
-			
+			var radius = mX * 1;
+			var stroke = mY / 2;
 		
 			// RESETS THE BACKGROUND
-			processing.background(224);	 
+			P.background(224);	 
 			
 			//Draw a circle
-			processing.strokeWeight(10);
-			processing.ellipse(mX, mY, 55, 55);
+			P.strokeWeight(stroke);
+			P.ellipse(mX, mY, radius, radius);
+			
+			//Draw image
+			P.tint(255, 128);
+   			P.image(b, 10, 10);
+    		P.image(b, 10, 10, 100, 100);
 			
 			var now = new Date();	 
 		
