2339ae49fcaed3d03c1b6974a652812be08ef067
Kellan <kellancraddock@gmail.com>
2011-01-18 13:39:05 -0800

    working on hover states

--- js/helpers/canvas.js
+++ js/helpers/canvas.js
@@ -2,68 +2,64 @@ $(function() {
 	function sketchProc(processing) {
 		var P = processing;
 		var b = P.PImage;
+		var bgcolor = 222;
+		
+		var cX = 0;
+		var cY = 0;
+		var cRadius = 40;
+		
+		var hover = false;
+		
 		//DEFAULT SETUP FUNCTION
 		P.setup = function() {
 			P.size(1024, 768);
-			mX = P.width / 2;  
-  			mY = P.height / 2;
+			cX = P.width / 2;  
+  			cY = P.height / 2;
   			// @pjs preload must be used to preload the image
    			/* @pjs preload="/images/CN70_360Tour_025T.jpg"; */
-   			/* var b = P.PImage; */
     		b = P.loadImage("/images/CN70_360Tour_025T.jpg");
 		}
 		//DEFAULT DRAW FUNCTION (loops every 60sec by default)	 
 		P.draw = function() {	
-			// determine center and max clock arm length	
-			var centerX = P.width / 2, centerY = P.height / 2;	
-			var maxArmLength = Math.min(centerX, centerY);
-			
-				
-		
-			function drawArm(position, lengthScale, weight) {	 
-				P.strokeWeight(weight);	 
-				P.line(centerX, centerY,  
-					 centerX + Math.sin(position * 2 * Math.PI) * lengthScale * maxArmLength,	
-					 centerY - Math.cos(position * 2 * Math.PI) * lengthScale * maxArmLength);	 
-					 
-				/*
-P.strokeWeight(10);
-				P.ellipse(55, 55, 55, 55);
-*/
-			}
-			
-			P.mouseDragged = function() {
-				mX = P.mouseX;
-				mY = P.mouseY;
-			}
-			var radius = mX * 1;
-			var stroke = mY / 2;
-		
 			// RESETS THE BACKGROUND
-			P.background(224);	 
+			P.background(bgcolor);
 			
+			//DETECT HOVER
+			if (P.mouseX > cX-cRadius && P.mouseX < cX+cRadius && P.mouseY > cY-cRadius && P.mouseY < cY+cRadius) {
+				hover = true;
+			}
+			
+			if (hover) {
+				if (cRadius < 70) {
+					cRadius = cRadius + 1;
+				} else {
+					cRadius = 70;
+				}
+			} else {
+				if (cRadius >= 70) {
+					cRadius = cRadius - 1;
+				} else {
+					cRadius = 40;
+				}
+			}
+			
+			/*
+P.mouseDragged = function() {
+				cX = P.mouseX;
+				cY = P.mouseY;
+			}
+*/
+			//cRadius = cX * 1;
+			var stroke = cY / 2;
+		
 			//Draw a circle
-			P.strokeWeight(stroke);
-			P.ellipse(mX, mY, radius, radius);
+			P.strokeWeight(2);
+			P.ellipse(cX, cY, cRadius, cRadius);
 			
 			//Draw image
 			P.tint(255, 128);
    			P.image(b, 10, 10);
     		P.image(b, 10, 10, 100, 100);
-			
-			var now = new Date();	 
-		
-			// Moving hours arm by small increments	 
-			var hoursPosition = (now.getHours() % 12 + now.getMinutes() / 60) / 12;	 
-			drawArm(hoursPosition, 0.5, 5);	 
-		
-			// Moving minutes arm by small increments	 
-			var minutesPosition = (now.getMinutes() + now.getSeconds() / 60) / 60;	
-			drawArm(minutesPosition, 0.80, 3);	
-		
-			// Moving hour arm by second increments	 
-			var secondsPosition = now.getSeconds() / 60;	
-			drawArm(secondsPosition, 0.90, 1);
 		
 		};	
 	}	 
